95eaa6ca18b15f36e5c6d14345271a7d7094f5aa
kamleshhv <kamleshvansajalia@gmail.com>
2024-01-12 16:51:42 +0000

    sweet_k6a: track repo with vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/kamleshhv/vendor_xiaomi_sweet_k6a.git vendor/xiaomi/sweet_k6a
+git clone --depth=1 https://github.com/Pulkit077/kernel_xiaomi_sweet_k6a.git kernel/xiaomi/sweet_k6a
