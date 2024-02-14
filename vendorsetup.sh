04c0ce5e8314fd458227ce4fea680e8ea6c760f6
kamleshhv <kamleshvansajalia@gmail.com>
2024-02-14 10:41:51 +0530

    sweet_k6a: switch to proton clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/kamleshhv/vendor_xiaomi_sweet_k6a.git vendor/xiaomi/sweet_k6a
 git clone --depth=1 https://github.com/Pulkit077/kernel_xiaomi_sweet_k6a.git kernel/xiaomi/sweet_k6a
 git clone -b v4a https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/proton-clang
