a74c43738daf5504f222a6b2f60aa67d10836797
kamleshhv <kamleshvansajalia@gmail.com>
2024-02-14 10:11:50 +0530

    sweet_k6a: fix some mess up

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,12 +69,12 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
-TARGET_KERNEL_CLANG_COMPILE := true TARGET_KERNEL_CONFIG := 
-vendor/sweet_user_defconfig TARGET_KERNEL_SOURCE := 
-kernel/xiaomi/sweet_k6a KERNEL_TOOLCHAIN_PATH := $(shell 
-pwd)/prebuilts/clang/host/linux-x86/proton-clang/bin/aarch64-linux-gnu- 
-KERNEL_LD := LD=ld.lld TARGET_KERNEL_CLANG_PATH := $(shell 
-pwd)/prebuilts/clang/host/linux-x86/proton-clang
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := vendor/sweet_user_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet_k6a
+KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/proton-clang/bin/aarch64-linux-gnu-
+KERNEL_LD := LD=ld.lld
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/proton-clang
 TARGET_KERNEL_CLANG_VERSION := proton
 
 # Lineage Health
