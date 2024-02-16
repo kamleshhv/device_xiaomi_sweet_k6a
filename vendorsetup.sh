be51e1616c2d08764bfb9f80236ebb56905a193a
Kamlesh <81345800+kamleshhv@users.noreply.github.com>
2024-02-16 09:11:46 +0530

    sweet_k6a: switch to kewl kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/proton-clang
 git clone https://github.com/kamleshhv/vendor_xiaomi_sweet_k6a.git vendor/xiaomi/sweet_k6a
-git clone --depth=1 https://github.com/Positron-B/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet_k6a
+git clone --depth=1 https://github.com/Pulkit077/kernel_xiaomi_sweet_k6a.git kernel/xiaomi/sweet_k6a
 git clone -b v4a https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
